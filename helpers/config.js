6d25348d97dcc9ae6a22cbc1183df46a037a19ab
Tom Wallyn <tom.wallyn@leroymerlin.fr>
2022-03-10 08:42:58 +0100

    fix(environment): production

--- helpers/config.js
+++ helpers/config.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 
 if(env === "development") {
     global.AUTH = {
@@ -10,9 +10,9 @@ if(env === "development") {
 
 } else if(env === "production") {
     global.AUTH = {
-        bdd_ip: 'db',
-        bdd_port: 3306,
-        url_front: 'http://localhost:8050',
+        bdd_ip: 'localhost',
+        bdd_port: 6033,
+        url_front: 'https://92.188.98.73:8061',
         env: "production"
     };
 
