a718fc9492d5d8326073776a9f8c26177fc618d5
tomwallyn <tomwallyntel@gmail.com>
2022-03-28 19:57:19 +0200

    feat(test): add test to application

--- helpers/config.js
+++ helpers/config.js
@@ -16,4 +16,12 @@ if(env === "development") {
         env: "production"
     };
 
+} else if(env === "test") {
+    global.AUTH = {
+        bdd_ip: 'db',
+        bdd_port: 3306,
+        url_front: 'https://92.188.98.73:8061',
+        env: "test"
+    };
+
 }
